b07379cb44882029e02048312743654f0ae2d6e0
Mark Barkell <markbarkell@gmail.com>
2017-04-29 20:55:07 -0400

    I've decided to use other tools than the Microsoft Visual Studio R Tools.

--- fitness-monitor/script.R
+++
@@ -1,9 +0,0 @@
-# Programmer: Mark Barkell
-# Purpose: Main entry point if using Microsoft Visual Studio 2015 R Tools for development. 
-
-# The Getting And Cleaning Data Course require the name of the program to be run_analysis.R
-# Microsoft Visual Studio 2015 with R Tools uses this script.R to source execute the 
-# R project.  So, this script.R just calls the name required by the course instructions.
-# It may be possible to make that button in Microsoft Visual Studio just point to the run_analysis.R,
-# but I have not yet found out how.
-source("../run_analysis.R")
