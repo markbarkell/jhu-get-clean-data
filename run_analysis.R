fa6204ea818024d9b784e441a983fb24c6c16678
Mark Barkell <markbarkell@gmail.com>
2017-05-06 11:36:42 -0400

    Removed the output CSV files which I had thought were needed for the completion of the assignment, but found that the assignment does not need them.  Changed the functionality of the run_analysis.R to read from the X_train.txt and the X_test.txt file.   I've not yet added in the mean and sum functionality over a group by.

--- run_analysis.R
+++ run_analysis.R
@@ -24,14 +24,24 @@ cSd <- function(listing) {
 }
 
 
+# This function retrieves the cells that are in a line.
+# Found that using the one or more spaces as the seperator
+# yields the results I had been expecting.
 featureLineSplit <- function (line) {
-  unlist(strsplit(line, " "))
+  unlist(strsplit(line, "\\s+", perl = TRUE))
 }
 
+# This function reads a bunch of lines and figures out the maximum number
+# of cells in the lines.
 featureMaxColumns <- function (lines) {
   max(sapply(lines, function(line) { length(featureLineSplit(line)) }))
 }
 
+# This function exists in order to read space seperated files two at a time (kindof).
+# and merge the results together.   I found that the read.table() parameters I was
+# passing where yielding many results inconsistent with what the data should be.  So,
+# I wrote my own file parser.   Yes, I know it is not the fastest reader, but its not terrible.
+# I could make it slower.
 featuresRead <- function (trainingFileName, testingFileName) {
   lines <- readLines(trainingFileName)
   lines <- append(lines, readLines(testingFileName))
@@ -52,136 +62,57 @@ featuresRead <- function (trainingFileName, testingFileName) {
     
 }
 
-
-zipDataFileName <- "datafile.zip"
-if (!file.exists(zipDataFileName)) {
-    download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile = zipDataFileName)
+if (!file.exists("UCI HAR Dataset")) {
+  zipDataFileName <- "datafile.zip"
+  if (!file.exists(zipDataFileName)) {
+      download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile = zipDataFileName)
+  }
+}
+if (!file.exists("UCI HAR Dataset")) {
+  unzip(zipDataFileName)
 }
 
-unzip(zipDataFileName)
-
-trainbodyAccXFileName <- 'UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt'
-trainbodyAccYFileName <- 'UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt'
-trainbodyAccZFileName <- 'UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt'
-traingyroXFileName <- 'UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt'
-traingyroYFileName <- 'UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt'
-traingyroZFileName <- 'UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt'
-traintotalAccXFileName <- 'UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt'
-traintotalAccYFileName <- 'UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt'
-traintotalAccZFileName <- 'UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt'
 trainsubjectFileName <- 'UCI HAR Dataset/train/subject_train.txt'
 trainactivityFileName <- 'UCI HAR Dataset/train/y_train.txt'
-
-
-testbodyAccXFileName <- 'UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt'
-testbodyAccYFileName <- 'UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt'
-testbodyAccZFileName <- 'UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt'
-testgyroXFileName <- 'UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt'
-testgyroYFileName <- 'UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt'
-testgyroZFileName <- 'UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt'
-testtotalAccXFileName <- 'UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt'
-testtotalAccYFileName <- 'UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt'
-testtotalAccZFileName <- 'UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt'
 
 testsubjectFileName <- 'UCI HAR Dataset/test/subject_test.txt'
 testactivityFileName <- 'UCI HAR Dataset/test/y_test.txt'
 activityLabelsFileName <- 'UCI HAR Dataset/activity_labels.txt'
 
-activities <- read.csv(activityLabelsFileName, header = FALSE, sep = " ")
+inputFeatureNamesFileName <- 'UCI HAR Dataset/features.txt'
 
-numericTrainActivities <- read.csv(trainactivityFileName, header = FALSE, sep = " ")
-numericTestActivities <- read.csv(testactivityFileName, header = FALSE, sep = " ")
+xtrainFilename <- 'UCI HAR Dataset/train/X_train.txt'
+xtestFilename <- 'UCI HAR Dataset/test/X_test.txt'
+
+
+
+inputFeatures <- read.table(inputFeatureNamesFileName, header = FALSE, sep = " ", stringsAsFactors = FALSE, as.is = TRUE)
+
+x <- featuresRead(xtrainFilename, xtestFilename)
+
+
+activities <- read.table(activityLabelsFileName, header = FALSE, sep = " ")
+
+numericTrainActivities <- read.table(trainactivityFileName, header = FALSE, sep = " ", blank.lines.skip = TRUE)
+numericTestActivities <- read.table(testactivityFileName, header = FALSE, sep = " ", blank.lines.skip =  TRUE)
 colnames(activities) <- c("IndexBy1", "Label")
 
 
-stringTrainActivities <- lapply(numericTrainActivities, function(x) { activities[x, "Label"] })
-stringTestActivities <- lapply(numericTestActivities, function(x) { activities[x, "Label"] })
+stringTrainActivities <- sapply(numericTrainActivities, function(x) { activities[x, "Label"] })
+stringTestActivities <- sapply(numericTestActivities, function(x) { activities[x, "Label"] })
 
 # Fully expect that this is slow, but dealing with such little data that slowness should not be too much of a problem: 
-stringActivities <- append(as.vector(stringTrainActivities$V1), as.vector(stringTestActivities$V1))
+stringActivities <- append(as.vector(stringTrainActivities), as.vector(stringTestActivities))
 
-trainSubjectIdentifiers <- read.csv(trainsubjectFileName, header = FALSE, sep = " ")
-testSubjectIdentifiers <- read.csv(testsubjectFileName, header = FALSE, sep = " ")
+trainSubjectIdentifiers <- read.table(trainsubjectFileName, header = FALSE, sep = " ")
+testSubjectIdentifiers <- read.table(testsubjectFileName, header = FALSE, sep = " ")
 
 subjectIdentifiers <- append(as.vector(trainSubjectIdentifiers$V1), as.vector(testSubjectIdentifiers$V1))
 
+df <- data.frame(Activity = stringActivities, Subject = subjectIdentifiers)
 
+lenX <- (dim(x)[2])
+for(i in 2:lenX) {
+  df[, inputFeatures$V2[i-1]] <- x[, i]
+}
 
-# Body Acceleration X, Y, and Z.
-accX <- featuresRead(trainbodyAccXFileName, testbodyAccXFileName)
-
-dAccX <- meanSd(accX)
-
-accY <- featuresRead(trainbodyAccYFileName, testbodyAccYFileName)
-
-dAccY <- meanSd(accY)
-
-accZ <- featuresRead(trainbodyAccZFileName, testbodyAccZFileName)
-
-dAccZ <- meanSd(accZ)
-
-
-# Body Gryoscopic measurements X, Y, and Z
-gyroX <- featuresRead(traingyroXFileName, testgyroXFileName)
-
-dGryoX <- meanSd(gyroX)
-
-gyroY <- featuresRead(traingyroYFileName, testgyroYFileName)
-
-dGyroY <- meanSd(gyroY)
-
-gyroZ <- featuresRead(traingyroZFileName, testgyroZFileName)
-
-dGyroZ <- meanSd(gyroZ)
-
-
-# Total Acceleration X, Y, and Z.
-taccX <- featuresRead(traintotalAccXFileName, testtotalAccXFileName)
-
-dtAccX <- meanSd(taccX)
-
-taccY <- featuresRead(traintotalAccYFileName, testtotalAccYFileName)
-
-dtAccY <- meanSd(taccY)
-
-taccZ <- featuresRead(traintotalAccZFileName, testtotalAccZFileName)
-
-dtAccZ <- meanSd(taccZ)
-
-
-
-df <- data.frame(Activity = stringActivities, Subject = subjectIdentifiers,
-                 BodyAccXMean = cMean(dAccX), BodyAccXSd = cSd(dAccX),
-                 BodyAccYMean = cMean(dAccY), BodyAccYSd = cSd(dAccY),
-                 BodyAccZMean = cMean(dAccZ), BodyAccZSd = cSd(dAccZ),
-                 BodyGyroXMean = cMean(dGryoX), BodayGyroXSd = cSd(dGryoX),
-                 BodyGyroYMean = cMean(dGyroY), BodayGyroYSd = cSd(dGyroY),
-                 BodyGyroZMean = cMean(dGyroZ), BodayGyroZSd = cSd(dGyroZ),
-                 BodyTotalAccXMean = cMean(dtAccX), BodyTotalAccXSd = cSd(dtAccX),
-                 BodyTotalAccYMean = cMean(dtAccY), BodyTotalAccYSd = cSd(dtAccY),
-                 BodyTotalAccZMean = cMean(dtAccZ), BodyTotalAccZSd = cSd(dtAccZ)
-                 )
-
-write.csv(df, "meansdperrow.csv")
-
-# It may be possible that this install needs to be done
-# on some new instances of R.  It is unlikely that an
-# R instance used alot will be missing plyr.
-# install.packages("plyr")
-
-library(plyr)
-
-df2 <- ddply(df, c("Activity", "Subject"),summarise, 
-             BodyAccXMean = mean(BodyAccXMean),
-             BodyAccYMean = mean(BodyAccYMean),
-             BodyAccZMean = mean(BodyAccZMean),
-             BodyGyroXMean = mean(BodyGyroXMean),
-             BodyGyroYMean = mean(BodyGyroYMean),
-             BodyGyroZMean = mean(BodyGyroZMean),
-             BodyTotalAccXMean = mean(BodyTotalAccXMean),
-             BodyTotalAccYMean = mean(BodyTotalAccYMean),
-             BodyTotalAccZMean = mean(BodyTotalAccZMean)
-             )
-
-write.csv(df2, "meanperactivitysubject.csv")
-write.table(df2, row.names = FALSE, "foo.txt")
